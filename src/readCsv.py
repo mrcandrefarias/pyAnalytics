8df536abbc432e2455581d05dd87e69d3169bd3e
Marcos Andre <marcos@getrak.com>
2017-05-16 20:59:20 -0300

    migrado leitura do csv para o pandas

--- src/readCsv.py
+++ src/readCsv.py
@@ -1,48 +1,13 @@
-import unicodecsv
+import pandas as pd
 
-def read_csv(filename):
-    with open(filename, 'rb') as f:
-        reader = unicodecsv.DictReader(f)
-        return list(reader)
+enrollments         = pd.read_csv('../data/enrollments.csv')
 
-def uniqueEnrollmentStudent(enrollments):
-    enrollment_students = set()
-    for enr in enrollments:
-        enrollment_students.add(enr['account_key'])
-    return enrollment_students;
+students = enrollments['account_key'].unique();
+for student in students:
+    print(student)
 
-def uniqueEngagementStudent(daily_engagement):
-    engagement_students = set()
-    for daily in daily_engagement:
-        engagement_students.add(daily['acct'])
-    return engagement_students
+daily_engagement    = pd.read_csv('../data/daily_engagement.csv')
 
-def uniqueSubmissionStudent(project_submissions):
-    submission = set()
-    for sub in project_submissions:
-        submission.add(sub['account_key'])
-    return submission
+print( len(daily_engagement['acct'].unique()) )
 
-def enrollmentNoEngagement(enrollments, uniqueEngagementStudent):
-    enrollmentNoEngagement = list()
-    for enr in enrollments:
-        student = enr['account_key']
-        if student not in uniqueEngagementStudent \
-            and enr['join_date'] != enr['cancel_date']:
-            enrollmentNoEngagement.append(enr)
-    return enrollmentNoEngagement
-
-
-
-
-enrollments         = read_csv('../data/enrollments.csv')
-daily_engagement    = read_csv('../data/daily_engagement.csv')
-project_submissions = read_csv('../data/project_submissions.csv')
-
-
-uniqueEnrollmentStudents = uniqueEnrollmentStudent(enrollments)
-uniqueEngagementStudent  = uniqueEngagementStudent(daily_engagement)
-uniqueSubmissionStudent  = uniqueSubmissionStudent(project_submissions)
-enrollmentNoEngagement = enrollmentNoEngagement(enrollments, uniqueEngagementStudent)
-
-print(len(enrollments))
+project_submissions = pd.read_csv('../data/project_submissions.csv')
