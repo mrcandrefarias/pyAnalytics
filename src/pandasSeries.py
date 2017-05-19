df3f6ad79fef8d5732822758839c54a86ea32bfc
Marcos André <marcos@localhost.localdomain>
2017-05-19 08:31:37 -0300

    addd funcoes

--- src/pandasSeries.py
+++ src/pandasSeries.py
@@ -18,7 +18,7 @@ gdp_values = [ 1681.61390973,   2155.48523109,  21495.80508273,    562.98768478,
 # Life expectancy and gdp data in 2007 for 20 countries
 life_expectancy = pd.Series(life_expectancy_values, index=countries)
 gdp             = pd.Series(gdp_values, index=countries)
-print (life_expectancy, gdp)
+
 
 def variable_correlation(v1, v2):
     both_above_mean         = (v1 > v1.mean()) & (v2 > v2.mean())
@@ -28,3 +28,33 @@ def variable_correlation(v1, v2):
     num_different_direction = len(v1) - num_same_direction
 
     return (num_same_direction, num_different_direction)
+
+print(variable_correlation(life_expectancy, gdp))
+
+countries = [
+    'Afghanistan', 'Albania', 'Algeria', 'Angola', 'Argentina',
+    'Armenia', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas',
+    'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium',
+    'Belize', 'Benin', 'Bhutan', 'Bolivia',
+    'Bosnia and Herzegovina'
+]
+
+
+employment_values = [
+    55.70000076,  51.40000153,  50.5       ,  75.69999695,
+    58.40000153,  40.09999847,  61.5       ,  57.09999847,
+    60.90000153,  66.59999847,  60.40000153,  68.09999847,
+    66.90000153,  53.40000153,  48.59999847,  56.79999924,
+    71.59999847,  58.40000153,  70.40000153,  41.20000076
+]
+
+# Employment data in 2007 for 20 countries
+employment = pd.Series(employment_values, index=countries)
+
+def max_employment(employment):
+    max_country = employment.argmax()
+
+    max_value   = employment.loc[max_country]
+    return (max_country, max_value)
+
+print ( max_employment(employment) )
