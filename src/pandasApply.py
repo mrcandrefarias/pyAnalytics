a1d80e7a77ab05bfc21aedcd41e3a2c6ea935566
Marcos André <marcos@localhost.localdomain>
2017-05-20 10:26:48 -0300

    adicionado arquivos

---
+++ src/pandasApply.py
@@ -0,0 +1,44 @@
+import pandas as pd
+
+if False:
+    s = pd.Series([1, 2, 3, 4, 5])
+    def add_one(x):
+        return x + 1
+    print s.apply(add_one)
+
+names = pd.Series([
+    'Andre Agassi',
+    'Barry Bonds',
+    'Christopher Columbus',
+    'Daniel Defoe',
+    'Emilio Estevez',
+    'Fred Flintstone',
+    'Greta Garbo',
+    'Humbert Humbert',
+    'Ivan Ilych',
+    'James Joyce',
+    'Keira Knightley',
+    'Lois Lane',
+    'Mike Myers',
+    'Nick Nolte',
+    'Ozzy Osbourne',
+    'Pablo Picasso',
+    'Quirinus Quirrell',
+    'Rachael Ray',
+    'Susan Sarandon',
+    'Tina Turner',
+    'Ugueth Urbina',
+    'Vince Vaughn',
+    'Woodrow Wilson',
+    'Yoji Yamada',
+    'Zinedine Zidane'
+])
+
+def reverse_name(name):
+    names = name.split(" ")
+    return names[1] + "," + names[0]
+
+def reverse_names(names):
+    return names.apply(reverse_name)
+
+print (reverse_names(names))
